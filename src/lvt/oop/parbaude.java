3e5851a8a200aca625b392d3389b1ae0cd20bfc3
Bogdans <bogdans.cumaks@sk.lvt.lv>
2025-10-14 16:07:28 +0300

    Izveidota parbaude

--- src/lvt/oop/parbaude.java
+++ src/lvt/oop/parbaude.java
@@ -1,5 +1,67 @@
 package lvt.oop;
 
-public class parbaude {
+import java.util.regex.Pattern;
 
+import javax.swing.JOptionPane;
+
+public class parbaude {
+	public static String virknesParbaude(String zinojums) {
+		String virkne;
+		do {
+			virkne = JOptionPane.showInputDialog(zinojums);
+			if(virkne == null)
+				return null;
+		}while(!Pattern.matches("^[\\p{L} .]+$", virkne));
+		return virkne;
+	}
+	public static int skaitlaParbaudeI(String zinojums, int min, int max) {
+		String ievade;
+		int skaitlis;
+		while(true) {
+			ievade = JOptionPane.showInputDialog(null, zinojums, 
+					"Datu ievade", JOptionPane.INFORMATION_MESSAGE); 
+			if(ievade == null)
+				return -1;
+			try {
+				skaitlis = Integer.parseInt(ievade);
+				if(skaitlis < min && skaitlis > max) {
+					JOptionPane.showMessageDialog(null, 
+					"Norādīts nederīgs intervāls", "Nekorekti dati",
+					JOptionPane.WARNING_MESSAGE);
+					continue;
+				}
+				
+				return skaitlis;
+			}catch(NumberFormatException e) {
+				JOptionPane.showMessageDialog(null,
+					"Netika ievadīts vesels skaitlis", "Nekorekti dati",
+					JOptionPane.WARNING_MESSAGE);
+			}
+		}
+	}
+		public static Double skaitlaParbaudeD(String zinojums, int min) {
+			String ievade;
+			Double skaitlis;
+			while(true) {
+				ievade = JOptionPane.showInputDialog(null, zinojums, 
+						"Datu ievade", JOptionPane.INFORMATION_MESSAGE); 
+				if(ievade == null)
+					return -1.0;
+				try {
+					skaitlis = Double.valueOf(ievade);
+					if(skaitlis < min) {
+						JOptionPane.showMessageDialog(null, 
+						"Norādīts nederīgs intervāls", "Nekorekti dati",
+						JOptionPane.WARNING_MESSAGE);
+						continue;
+					}
+					
+					return skaitlis;
+				}catch(NumberFormatException e) {
+					JOptionPane.showMessageDialog(null,
+						"Netika ievadīts vesels skaitlis", "Nekorekti dati",
+						JOptionPane.WARNING_MESSAGE);
+				}
+			}
+	}
 }
