47f218e6626336dc3915f6cc1eb06ff4a99b92ad
Bogdans <bogdans.cumaks@sk.lvt.lv>
2025-10-14 16:05:44 +0300

    Uzlaboju problemu ar kalnuriteni

--- src/lvt/oop/KalnuRitenis.java
+++ src/lvt/oop/KalnuRitenis.java
@@ -1,36 +1,31 @@
 package lvt.oop;
 
-public class KalnuRitenis extends Velosipeds {
-	// Atributi
+public class KalnuRitenis extends Velosipeds{
 	private int iestatAtrums;
 	private boolean atsperes;
 	
-	//Konstruktors
-	public KalnuRitenis(int iestatAtrums, boolean atsperes, 
+	//konstruktors
+	public KalnuRitenis(int iestatAtrums, boolean atsperes,
 			int ritenaD, int sedeklaPoz, double cena, String razotajs) {
-		super( ritenaD,  sedeklaPoz,  cena,  razotajs);//Lai ņemt no citas klases
+		super(ritenaD, sedeklaPoz, cena, razotajs);
 		this.iestatAtrums = iestatAtrums;
 		this.atsperes = atsperes;
 	}
 	
-	//Metodes
+	//metodes
 	public int noteiktIestatAtr() {
 		return iestatAtrums;
 	}
-	
 	public void parslegtAtr(int atr) {
 		iestatAtrums = atr;
 	}
-	
 	public String izvadit() {
 		return "Riteņa ražotājs: "+noteiktRaz()+
-				"\nRiteņa cena: "+noteiktCenu()+" EUR"+
-				"\nRiteņa lielums collās: "+noteiktRitenaD()+
-				"\nIestatītais sēdekļa augstums: "+noteiktSedeklaPoz()+
+				"\nRiteņa cena: "+noteiktCenu()+
+				"\nRiteņa leilums collās: "+noteiktRitenaD()+
+				"\nIEstatītais sēdekļa garums: "+noteiktSedeklaPoz()+
 				"\nKustības ātrums: "+noteiktAtr()+"m/s"+
 				"\nIestatītais ātrums: "+noteiktIestatAtr()+
-				"\nPapildus amortizacija: "+((atsperes)? "Ir" : "Nav");
+				"\nPapildus amortizācija: "+((atsperes)? "Ir": "Nav");
 	}
- 
-		
 }
--- src/lvt/oop/Velosipeds.java
+++ src/lvt/oop/Velosipeds.java
@@ -1,5 +1,70 @@
 package lvt.oop;
 
-public class Velosipeds {
+import java.util.Random;
 
+public class Velosipeds {
+	//atribūti
+	private int ritenaD, sedeklaPoz;
+	private double atrumsMS, cena;
+	private String razotajs;
+	
+	//kostruktors
+	public Velosipeds(int ritenaD, int sedeklaPoz,
+				double cena, String razotajs) {
+		this.ritenaD = ritenaD;
+		this.sedeklaPoz = sedeklaPoz;
+		atrumsMS = kustiba();
+		this.cena = cena;
+		this.razotajs = razotajs;
+	}
+
+	//metodes
+	public int noteiktRitenaD() {
+		return ritenaD;
+	}
+	
+	public int noteiktSedeklaPoz() {
+		return sedeklaPoz;
+	}
+	
+	public String noteiktRaz() {
+		return razotajs;
+	}
+	
+	public double noteiktCenu() {
+		return cena;
+	}
+	
+	public double kustiba() {
+		Random rand = new Random();
+		double celsM, laiksS;
+		celsM = rand.nextDouble()*(1000-1)+1;
+		laiksS = rand.nextDouble()*(60-10)+10;
+		return Math.round(celsM/laiksS);
+	}
+	public double noteiktAtr() {
+		return atrumsMS;
+	}
+	
+	public String izvadit() {
+		return "Riteņa ražotājs: "+noteiktRaz()+
+				"\nRiteņa cena: "+noteiktCenu()+
+				"\nRiteņa leilums collās: "+noteiktRitenaD()+
+				"\nIEstatītais sēdekļa garums: "+noteiktSedeklaPoz()+
+				"\nKustības ātrums: "+noteiktAtr()+"m/s";
+	}
+	
+	public void iestatitSedeklaPoz(int pozicija) {
+		sedeklaPoz = pozicija;
+	}
+	public void mities(int minienuSk) {
+		atrumsMS += noteiktAtr() + (0.15 * minienuSk);
+	}
+	public void bremzet(int berze) {
+		if(noteiktAtr() > 0) {
+			atrumsMS -= (noteiktAtr() *0.55)*berze;
+			if(atrumsMS < 0)
+				atrumsMS = 0;
+		}
+	}
 }
